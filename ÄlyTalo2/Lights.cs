97bd90c5c43f12cfc73cbafdd6c9a79250865c40
Joona Janhunen <joona.janhunen@stud.pointcollege.fi>
2018-05-20 15:28:22 +0300

    Alytalo Githubiin

---
+++ ÄlyTalo2/Lights.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÄlyTalo2
+{
+    public class Lights
+    {
+        public bool Switched { get; set; }
+        public string Dimmer { get; set; }
+
+        public void LightsOn()
+        {
+            Switched = true;
+        }
+        public void LightsOff()
+        {
+            Switched = false;
+        }
+
+    }
+}
---
+++ ÄlyTalo2/Sauna.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÄlyTalo2
+{
+    public class Sauna
+    {
+        public bool Switched { get; set; }
+        public int SaunaTemperature { get; set; }
+
+        public void SaunaOn()
+        {
+            Switched = true;
+        }
+        public void SaunaOff()
+        {
+            Switched = false;
+        }
+    }
+
+
+}
+
