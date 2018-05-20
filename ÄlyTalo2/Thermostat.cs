f75c34ff637ccef58c3d7da90d653be7f30ce6cf
Joona Janhunen <joona.janhunen@stud.pointcollege.fi>
2018-05-20 15:47:42 +0300

    Revert "Revert "Alytalo Githubiin""
    This reverts commit 02391eff22f372641c6b43b4d564e93d4894f803.

---
+++ ÄlyTalo2/Thermostat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÄlyTalo2
+{
+    public class Thermostat
+    {
+        public int Temperature { get; set; }
+
+        public void SetTemperature(string GoalTemperature)
+        {
+            try
+            {
+                Temperature = int.Parse(GoalTemperature);
+
+            }
+            catch (Exception)
+            {
+                
+            }
+            
+        }
+
+    }
+}
